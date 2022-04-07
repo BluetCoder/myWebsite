e4319eebae8987edc4348c055518e6f3ddc931d6
Joshua <joshsy18@gmail.com>
2022-04-07 13:50:05 +1000

    draft 1

---
+++ website_portfolio/js/main.js
@@ -0,0 +1,12 @@
+// function typeWriter() {
+//     console.log("I'm running");
+//     var i = 0;
+//     var txt = 'Hi, my name is Joshua Sy';
+//     var speed = 50;
+//     if (i < txt.length) {
+//         document.getElementsByClassName("heading").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
+// typeWriter();
